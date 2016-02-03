07fcd5b6ff00214fbd5110c49b3fb2fcafab9c6d
Chathuranga <UCHATNU@PEROOT.COM>
2016-02-03 11:29:32 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports={
+
+	entry:"./src/js/main.js",
+	output:{
+		path:'./dist',
+		filename:"bundle.js",
+		publicPath:'/'
+	},
+	devServer:{
+		inline:true,
+		contentBase:'./dist'
+	},
+	module:
+		{
+			loaders:[
+				{
+					test:/\.jsx?$/,
+					exclude:/(node_modules|bower_components)/,
+					loader:'babel',
+					query:{
+						presets:['react','es2015']
+					}
+				}
+			]
+		}
+};
+
+
+
